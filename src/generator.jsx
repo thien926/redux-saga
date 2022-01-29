488c7ff5e38d66757a795c8a3c7c4cb8d8d70a6d
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2022-01-29 15:14:51 +0700

    second commit

--- src/generator.jsx
+++ src/generator.jsx
@@ -1,25 +1,47 @@
-console.log('==================== learning generator function ============================');
+// console.log('==================== learning generator function ============================');
+// function* helloGeneratorFunction() {
+//     yield 2019;
+//     return 'Tự học lập trình redux-saga';
+// }
 
-function* helloGeneratorFunction() {
-    yield 2019;
-    return 'Tự học lập trình redux-saga';
+// const result = helloGeneratorFunction();    //iterator
+// console.log('result 1: ', result.next());
+// console.log('result 2: ', result.next());
+// console.log('result 3: ', result.next());
+// console.log('==================== done generator function ===============================');
+
+
+// console.log('==================== vòng lặp vô tận ============================');
+// function* helloGeneratorFunction() {
+//     while (true) {
+//         yield 'Tôi đang lắng nghe...';
+//     }
+// }
+
+// const iterator = helloGeneratorFunction();
+// console.log('Result 1: ', iterator.next());
+// console.log('Result 2: ', iterator.next());
+// console.log('==================== done vòng lặp vô tận ===============================');
+
+
+console.log('==================== generator function: generator trong generator ============================');
+function* printName() {
+    yield 'redux-saga';
 }
 
-const result = helloGeneratorFunction();    //iterator
-console.log('result 1: ', result.next());
-console.log('result 2: ', result.next());
-console.log('result 3: ', result.next());
+function* hello() {
+    yield 'xin chào';
 
-// console.log('result 1: ', helloGeneratorFunction().next());
-// console.log('result 2: ', helloGeneratorFunction().next());
-// console.log('result 3: ', helloGeneratorFunction().next());
+    // const nameIterator = printName();
+    // yield nameIterator.next();
 
+    yield* printName();
 
+    yield '. Kết thúc.';
+}
 
-
-
-
-
-
-
-console.log('==================== done generator function ===============================');
+const iterator = hello();
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log('==================== done generator function: generator trong generator ===============================');
